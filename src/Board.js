3fb85efe5b595003b68115c67c6379091f476c75
Peng.Cao <pcao@thoughtworks.com>
2018-09-01 14:04:07 +0800

    Lifting state up to Game

--- src/Board.js
+++ src/Board.js
@@ -4,38 +4,17 @@ import Square from './FunctionSquare'
 import FSquare from './FunctionSquare'
 import calculateWinner from './calculateWinner'
 class Board extends React.Component{
-    constructor(props) {
-        super(props);
-        this.state = {
-            squares: Array(9).fill(null),
-            xIsNext: true,
-        };
-    }
-
-    handleClick(i) {
-        const squares = this.state.squares.slice();
-        squares[i] = this.state.xIsNext ? 'X' : 'O';
-        this.setState({
-            squares: squares,
-            xIsNext: !this.state.xIsNext,
-        });
-    }
-
     renderSquare(i){
         return(
-            <FSquare value={this.state.squares[i]}
-                onClick={() => this.handleClick(i)}
+            <FSquare value={this.props.squares[i]}
+                onClick={() => this.props.onClick(i)}
         />
         );
     }
+
     render(){
-        const winner = calculateWinner(this.state.squares)
-        const status = winner ?
-            'Winner: ' + winner :
-            'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
         return(
             <div>
-                <div className="status">{status}</div>
                 <div className="board-row">
                     {this.renderSquare(0)}
                     {this.renderSquare(1)}
