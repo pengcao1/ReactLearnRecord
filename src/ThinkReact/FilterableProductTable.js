0dbbdd2c3ccd0602c8584b4d82f669bae9186212
Peng.Cao <pcao@thoughtworks.com>
2018-09-03 21:46:08 +0800

    add ProductTable

--- src/ThinkReact/FilterableProductTable.js
+++ src/ThinkReact/FilterableProductTable.js
@@ -1,11 +1,13 @@
 import React from "react"
 import SearchBar from "./SearchBar";
+import ProductTable from "./ProductTable";
 
 class FilterableProductTable extends React.Component {
     render(){
         return(
             <div>
-                <SearchBar></SearchBar>
+                <SearchBar/>
+                <ProductTable/>
             </div>
         );
     }
